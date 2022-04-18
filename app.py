5421d8d187ff7126b7a79d11f0fe67c884c643b9
Ambuj Gupta <ambujgupta2000@gmail.com>
2022-04-18 22:54:08 +0530

    Update

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from distutils.log import debug
 from flask import Flask,render_template,url_for,request,session,logging,redirect,flash
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session,sessionmaker
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import scoped_session,sessionmaker
 import os
 
-engine=create_engine("mysql+pymysql://root:@localhost/users")
-db=scoped_session(sessionmaker(bind=engine))
+# engine=create_engine("mysql+pymysql://root:@localhost/users")
+# db=scoped_session(sessionmaker(bind=engine))
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
 
 
 @app.route('/')
@@ -32,12 +32,10 @@ def about():
 def internal_error(error):
     return render_template('500.html'), 500
 
-
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
 
-
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5001))
+    port = int(os.environ.get('PORT', 5100))
     app.run(host='0.0.0.0', port=port)
